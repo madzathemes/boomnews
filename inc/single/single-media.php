1f3cb8b90e0bf8ff1260dea0a2aad2f414671883
Madars Bitenieks <madza@madzathemes.com>
2016-12-12 17:32:37 +0200

    fix

--- inc/single/single-media.php
+++ inc/single/single-media.php
@@ -6,7 +6,7 @@
 
   $video = "";
   if($videos!=""){
-  	$video = do_shortcode("[embed]".$videos."[/embed]");
+  	$video = apply_filters('the_content', "[embed]".$videos."[/embed]");
   }
 
   if(has_post_format("gallery")) { if(!empty($images)) { ?>
