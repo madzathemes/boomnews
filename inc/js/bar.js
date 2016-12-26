e0d3a33f4785c964788b5da99e508818d78e7bdc
Madars Bitenieks <madza@madzathemes.com>
2016-12-26 03:02:22 +0200

    fix

---
+++ inc/js/bar.js
@@ -0,0 +1,46 @@
+jQuery(document).ready(function() {
+  'use strict';
+
+  jQuery(".mtc-select-border .mct-box").on("click",function() {
+    jQuery(".mtc-select-border .mct-box").removeClass("active");
+    jQuery(this).addClass("active");
+    var $border = jQuery(this).data( "border" );
+    jQuery("body").removeClass("mt-radius-25");
+    jQuery("body").removeClass("mt-radius-5");
+    jQuery('body').addClass($border);
+  });
+
+  jQuery(".mtc-select-sheme .mct-box").on("click",function() {
+    jQuery(".mtc-select-sheme .mct-box").removeClass("active");
+    jQuery(this).addClass("active");
+    var $color = jQuery(this).data( "sheme" );
+    jQuery("body").removeClass("mt-color-1");
+    jQuery("body").removeClass("mt-color-2");
+    jQuery('body').addClass($color);
+  });
+
+  jQuery(".mtc-select-layout .mtc-bt").on("click",function() {
+    jQuery(".mtc-select-layout .mtc-bt").removeClass("active");
+    jQuery(this).addClass("active");
+    var $layout = jQuery(this).data( "layout" );
+    jQuery("body").removeClass("boxed-layout-on");
+    jQuery('body').addClass($layout);
+  });
+
+  jQuery(".mtc-select-animation .mtc-bt").on("click",function() {
+    jQuery(".mtc-select-animation .mtc-bt").removeClass("active");
+    jQuery(this).addClass("active");
+    var $layout = jQuery(this).data( "animation" );
+    jQuery("body").removeClass("zoom-on");
+    jQuery('body').addClass($layout);
+  });
+
+  jQuery(".mtc-close").on("click",function() {
+    jQuery('.mtc, .mtc-fix').addClass("active");
+  });
+
+  jQuery('.mtc,.mtc-d').hover(
+    function(){jQuery('.mtc-d').toggleClass('active');}
+  );
+
+});
--- inc/js/defer.js
+++ inc/js/defer.js
@@ -45,42 +45,6 @@ function hasScrolled() {
 jQuery(document).ready(function() {
   'use strict';
 
-
-  jQuery(".mtc-select-border .mct-box").on("click",function() {
-    jQuery(".mtc-select-border .mct-box").removeClass("active");
-    jQuery(this).addClass("active");
-    var $border = jQuery(this).data( "border" );
-    jQuery("body").removeClass("mt-radius-25");
-    jQuery("body").removeClass("mt-radius-5");
-    jQuery('body').addClass($border);
-  });
-
-  jQuery(".mtc-select-sheme .mct-box").on("click",function() {
-    jQuery(".mtc-select-sheme .mct-box").removeClass("active");
-    jQuery(this).addClass("active");
-    var $color = jQuery(this).data( "sheme" );
-    jQuery("body").removeClass("mt-color-1");
-    jQuery("body").removeClass("mt-color-2");
-    jQuery('body').addClass($color);
-  });
-  jQuery(".mtc-select-layout .mtc-bt").on("click",function() {
-    jQuery(".mtc-select-layout .mtc-bt").removeClass("active");
-    jQuery(this).addClass("active");
-    var $layout = jQuery(this).data( "layout" );
-    jQuery("body").removeClass("boxed-layout-on");
-    jQuery('body').addClass($layout);
-  });
-  jQuery(".mtc-select-animation .mtc-bt").on("click",function() {
-    jQuery(".mtc-select-animation .mtc-bt").removeClass("active");
-    jQuery(this).addClass("active");
-    var $layout = jQuery(this).data( "animation" );
-    jQuery("body").removeClass("zoom-on");
-    jQuery('body').addClass($layout);
-  });
-  jQuery(".mtc-close").on("click",function() {
-    jQuery('.mtc, .mtc-fix').addClass("active");
-  });
-
   jQuery(".nav-search-wrap, .nav-search").on("click",function() {
     jQuery('.header-menu, .mt-header-mobile').addClass("active");
   });
@@ -91,12 +55,7 @@ jQuery(document).ready(function() {
   jQuery('.nav-search-wrap, .search-close').hover(
     function(){jQuery(this).toggleClass('hover');}
   );
-  jQuery('.mtc,.mtc-d').addClass("loaded");
-  jQuery('.mtc.loaded,.mtc-d.loaded').mouseover(
-    function(){jQuery('.mtc-d.loaded').toggleClass('active');}
-  );
-
-
+  
   jQuery(".mt-tabc").on("click", function() {
     var tab = jQuery(this).data( "tab" );
     jQuery(".mt-tab,.mt-tabc").removeClass("active");
